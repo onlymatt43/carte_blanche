82877a9ed3cb8672d2f7d83407d8bf21fff4a2fa
onlymatt43 <160356311+onlymatt43@users.noreply.github.com>
2025-07-26 05:24:42 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -88,7 +88,10 @@ def validate_code():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+import os
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
 
 
 from flask import redirect, url_for
